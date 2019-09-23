36d6df22b876d7b16f5220e801d87717889822d1
andrew dillon <majik895@gmail.com>
2019-09-23 15:39:53 -0400

    :8ball: initial commit

---
+++ knexfile.js
@@ -0,0 +1,38 @@
+module.exports = {
+	development: {
+		client: 'sqlite3',
+		connection: {
+			filename: './database/soup-kitchen-db.sqlite3'
+		},
+		useNullAsDefault: true,
+		migrations: {
+			directory: './database/migrations'
+		},
+		seeds: {
+			directory: './database/seeds'
+		}
+	},
+	testing: {
+		client: 'sqlite3',
+		connection: {
+			filename: './database/soup-kitchen-db.sqlite3'
+		},
+		useNullAsDefault: true,
+		migrations: {
+			directory: './database/migrations'
+		},
+		seeds: {
+			directory: './database/seeds'
+		}
+	}
+	// production: {
+	//   client: 'sqlite3',
+	//   migrations: {
+	//     directory: './database/migrations'
+	//   },
+	//   seeds: {
+	//     directory: './database/seeds'
+	//   },
+	//   useNullAsDefault: true
+	// }
+}
